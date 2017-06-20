d9420d92eb7cfdcb5287dd9cf5ac0609e0c80be4
lleiou <leoliu0723@gmail.com>
2017-06-19 23:17:57 -0400

    added some links that deal with the same data set.

--- data-cleaning.py
+++ data-cleaning.py
@@ -5,17 +5,22 @@
 # http://bit.ly/2sjjdlR
 
 import pandas
-artworks_100 = pandas.read_csv("https://media.githubusercontent.com/media/metmuseum/openaccess/master/MetObjects.csv")
+data = pandas.read_csv("https://media.githubusercontent.com/media/metmuseum/openaccess/master/MetObjects.csv")
 # alternative way:
 # artworks_100 = pandas.read_csv("/Users/lleiou/Google Drive/Python/openaccess/MetObjects.csv")
-artworks_100.head(10)
+sample = artworks_100.head(10)
+# data
+sample["Repository"].value_counts()
+data["Culture"].value_counts()
 
 
 
 
-
-
-
-# about large file, like the csv file in this problem...
+# about large file,  like the csv file in this problem...
 # type the following code in termial:
 # curl -O https://media.githubusercontent.com/media/metmuseum/openaccess/master/MetObjects.csv
+
+# some articles on the same data set:
+# http://sqlservercode.blogspot.com/2017/02/importing-metropolitan-museum-of-arts.html
+# https://www.springboard.com/blog/ggplot2-in-r-tutorial/
+# https://avital.ca/notes/exploring-met-museums-openaccess-dataset
